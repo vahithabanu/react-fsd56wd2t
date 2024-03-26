e393acefa09492f730620f9db29af483ddb3cf7f
svahitha25 <svahitha25@gmail.com>
2024-03-26 20:59:57 +0530

    inline style to a variable object

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -4,8 +4,12 @@ const Hello =(props) =>{
 
     //javascript code
 //console.log(props);
+const headerStyle={
+     color:'red',
+     fontFamily:'monospace'
+}
     return(
-      <h1 style={{ color:'red',fontFamily:'monospace'}}>Hello {props.name}</h1>
+      <h1 style={headerStyle}>Hello {props.name}</h1>
     )
   }
   
